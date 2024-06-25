4653aa2359d40a05ec821b813ef7fdd4aacf58c8
Sohail Sayed <sayedsohail150@gmail.com>
2024-06-25 15:23:35 +0530

    first commit

--- src/components/MiddleSectionIcons.js
+++ src/components/MiddleSectionIcons.js
@@ -2,7 +2,7 @@
 'use client'
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSkype, faLine } from '@fortawesome/free-brands-svg-icons';
+import { faSkype, faLine, faWhatsapp } from '@fortawesome/free-brands-svg-icons'; // Import WhatsApp icon
 import styles from '../app/styles/middleSectionIcons.module.css';
 
 const MiddleSectionIcons = () => {
@@ -14,13 +14,19 @@ const MiddleSectionIcons = () => {
         window.open('your line link'); // Replace with actual Line link
     };
 
+    const openWhatsapp = () => {
+        window.open('https://wa.me/9823316450'); // Replace with actual WhatsApp link
+    };
+
     return (
         <div className={styles.wrapper}> 
-        <div className={`${styles.middleSection} ${styles.sticky}`}>
-            <FontAwesomeIcon icon={faSkype} className={`${styles.icon} ${styles.skype}`} onClick={openSkype} />
-            <FontAwesomeIcon icon={faLine} className={`${styles.icon} ${styles.line}`} onClick={openLine} />
-        </div></div>
+            <div className={`${styles.middleSection} ${styles.sticky}`}>
+                <FontAwesomeIcon icon={faSkype} className={`${styles.icon} ${styles.skype}`} onClick={openSkype} />
+                <FontAwesomeIcon icon={faLine} className={`${styles.icon} ${styles.line}`} onClick={openLine} />
+                <FontAwesomeIcon icon={faWhatsapp} className={`${styles.icon} ${styles.whatsapp}`} onClick={openWhatsapp} />
+            </div>
+        </div>
     );
-}                           
+};
 
 export default MiddleSectionIcons;
