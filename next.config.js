f1c25a6ef9dc3df45b7ba52b393d032c1e4ef954
Sohail Sayed <sayedsohail150@gmail.com>
2024-06-25 15:04:47 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    distDir: '.next-build',
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  // Enable SWC minification only in production build
+  swcMinify: PHASE_PRODUCTION_BUILD ? true : false,
+};
+
+module.exports = nextConfig;
